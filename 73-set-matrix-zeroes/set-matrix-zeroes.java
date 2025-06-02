df628b64323decd8784fdcd98935a287a29f21ed
RashmikaKashyap <141344239+RashmikaKashyap@users.noreply.github.com>
2025-06-03 01:27:05 +0530

    Time: 2 ms (17.87%) | Memory: 45.6 MB (80.93%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -1,6 +1,56 @@
 import java.util.Arrays;
 class Solution
 {
+    public void setZeroes(int[][] matrix)
+    {
+        int n=matrix.length;
+        int m=matrix[0].length;
+        //int row[]=new int[n]; matrix[...][0] row keeps changing & 0th col{HORIZON}
+        //int col[]=new int[m]; matrix[0][...] col keeps changing & 0th row{VERTICAL}
+
+        int col0=1; // flag for the 0th colmn
+        for(int i=0;i<n;i++)
+        {
+            if(matrix[i][0]==0)  col0=0; 
+            for(int j=1;j<m;j++)
+            {
+                if(matrix[i][j]==0)
+                {   
+                    matrix[i][0]=0; //marking row
+                    matrix[0][j]=0; //marking colmn
+                }
+            }   
+        }
+        for(int i = 1; i < n; i++) 
+        {
+            for (int j = 1; j < m; j++) 
+            {
+                if(matrix[i][0]==0 || matrix[0][j]==0)
+                matrix[i][j]=0;
+            }
+        }
+
+        if(matrix[0][0]==0)
+        {
+            for(int j=0;j<m;j++)
+            {
+                matrix[0][j]=0;
+            }
+        }
+
+        if(col0==0)
+        {
+            for(int i=0;i<n;i++)
+            {
+                matrix[i][0]=0;
+            }
+        }
+
+
+    }
+}
+/*class Solution
+{
     public void setZeroes(int[][] matrix) 
     {
         int n=matrix.length;
@@ -29,7 +79,7 @@ class Solution
         }
     }
 }
-
+*/
 /*class Solution {
     public void setZeroes(int[][] matrix) 
     {
