ceb252c120d6b216bb502243678e35fbe49fb41e
RashmikaKashyap <141344239+RashmikaKashyap@users.noreply.github.com>
2025-06-03 01:01:59 +0530

    Time: 1 ms (72.36%) | Memory: 45.6 MB (80.90%) - LeetSync

---
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -0,0 +1,78 @@
+import java.util.Arrays;
+class Solution
+{
+    public void setZeroes(int[][] matrix) 
+    {
+        int n=matrix.length;
+        int m=matrix[0].length;
+        int row[]=new int[n];
+        int col[]=new int[m];
+
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                if(matrix[i][j]==0)
+                {
+                    row[i]=1;
+                    col[j]=1;
+                }
+            }
+        }
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                if(row[i]==1 || col[j]==1)
+                matrix[i][j]=0;
+            }
+        }
+    }
+}
+
+/*class Solution {
+    public void setZeroes(int[][] matrix) 
+    {
+        int n =matrix.length;
+        int m=matrix[0].length;
+        int row[]=new int [n];
+        int col[]=new int [m];
+
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                if(matrix[i][j]==0)
+                {
+                    markRow(matrix,i,j,n,m);
+                    markCol(matrix,i,j,n,m);
+                }
+            }
+        }
+
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                if(matrix[i][j]==-1)
+                matrix[i][j]=0;
+            }
+        }
+    }
+    void markRow(int matrix[][],int i,int col,int n,int m)
+        {
+            for(int j=0;j<m;j++)
+            {
+                if(matrix[i][j]!=0)
+                matrix[i][j]=-1;
+            }
+        }
+    void markCol(int matrix[][],int row,int j,int n,int m)
+        {
+            for(int i=0;i<n;i++)
+            {
+                if(matrix[i][j]!=0)
+                matrix[i][j]=-1;
+            }
+        }
+}*/
